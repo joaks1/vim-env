81dd5b89f4d6ea502e8d79e5fc75090a803f77b5
Jamie Oaks <joaks1@gmail.com>
2012-11-10 23:52:49 -0600

    Adding setup script.

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+this_dir=`dirname $0`
+
+if [ $this_dir = '.' ]
+then
+    base_dir=`pwd`
+else
+    cd $this_dir
+    base_dir=`pwd`
+fi
+
+for vim_file in ${base_dir}/*vimrc
+do
+    dest_name="."$(basename $vim_file)
+    dest_path=${HOME}/${dest_name}
+    if [ ! -e $dest_path ]
+    then
+        ln -s $vim_file $dest_path
+        echo "Successfully linked $vim_file to $dest_file"
+    else
+        echo "WARNING: the file $dest_path exists... skipping!"
+    fi
+done
+
+# initiate submodules
+cd $base_dir
+git submodule init
+git submodule update
+
