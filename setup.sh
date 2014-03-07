f8505912b1e2f8d4a7c19cc812720e7ea4d014c3
Jamie Oaks <joaks1@gmail.com>
2014-03-06 22:41:43 -0800

    Updating `setup.sh` script.
    Quoting variables so that paths with spaces do not bork the script.

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-this_dir=`dirname $0`
+this_dir="`dirname $0`"
 
-if [ $this_dir = '.' ]
+if [ "$this_dir" = '.' ]
 then
-    base_dir=`pwd`
+    base_dir="`pwd`"
 else
-    cd $this_dir
-    base_dir=`pwd`
+    cd "$this_dir"
+    base_dir="`pwd`"
 fi
 
-for vim_file in ${base_dir}/*vimrc
+for vim_file in "$base_dir"/*vimrc
 do
-    dest_name="."$(basename $vim_file)
-    dest_path=${HOME}/${dest_name}
-    if [ ! -e $dest_path ]
+    dest_name="."$(basename "$vim_file")
+    dest_path="$HOME/$dest_name"
+    if [ ! -e "$dest_path" ]
     then
-        ln -s $vim_file $dest_path
+        ln -s "$vim_file" "$dest_path"
         echo "Successfully linked $vim_file to $dest_file"
     else
         echo "WARNING: the file $dest_path exists... skipping!"
@@ -24,7 +24,7 @@ do
 done
 
 # initiate submodules
-cd $base_dir
+cd "$base_dir"
 git submodule init
 git submodule update
 
