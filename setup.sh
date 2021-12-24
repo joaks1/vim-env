0d3a66df2c72a01f9a6fff839bad97f7db32ca1e
Jamie R. Oaks <joaks1@gmail.com>
2021-12-24 15:40:00 -0600

    Removing neovim bit from setup script.

--- setup.sh
+++ setup.sh
@@ -21,24 +21,6 @@ do
     else
         echo "WARNING: the file $dest_path exists... skipping!"
     fi
-
-    # Put vimrc in place for NeoVim
-    if [ "$dest_name" = ".vimrc" ]
-    then
-        nvim_dir="${HOME}/.config/nvim"
-        if [ ! -e "$nvim_dir" ]
-        then
-            mkdir -p "$nvim_dir"
-        fi
-        dest_path="${nvim_dir}/init.vim"
-        if [ ! -e "$dest_path" ]
-        then
-            ln -s "$vim_file" "$dest_path"
-            echo "Successfully linked $vim_file to $dest_path"
-        else
-            echo "WARNING: the file $dest_path exists... skipping!"
-        fi
-    fi
 done
 
 # initiate submodules
