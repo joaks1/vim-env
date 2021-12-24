7d1e85fbc9aa8ac734338200da1026de68ba42f8
Jamie R. Oaks <joaks1@gmail.com>
2021-12-24 15:27:33 -0600

    Updating setup script for neovim.
    Linking vimrc to neovim config path.

--- setup.sh
+++ setup.sh
@@ -13,13 +13,31 @@ fi
 for vim_file in "$base_dir"/*vimrc
 do
     dest_name="."$(basename "$vim_file")
-    dest_path="$HOME/$dest_name"
+    dest_path="${HOME}/${dest_name}"
     if [ ! -e "$dest_path" ]
     then
         ln -s "$vim_file" "$dest_path"
-        echo "Successfully linked $vim_file to $dest_file"
+        echo "Successfully linked $vim_file to $dest_path"
     else
         echo "WARNING: the file $dest_path exists... skipping!"
+    fi
+
+    # Put vimrc in place for NeoVim
+    if [ "$dest_name" = ".vimrc" ]
+    then
+        nvim_dir="${HOME}/.config/nvim"
+        if [ ! -e "$nvim_dir" ]
+        then
+            mkdir -p "$nvim_dir"
+        fi
+        dest_path="${nvim_dir}/init.vim"
+        if [ ! -e "$dest_path" ]
+        then
+            ln -s "$vim_file" "$dest_path"
+            echo "Successfully linked $vim_file to $dest_path"
+        else
+            echo "WARNING: the file $dest_path exists... skipping!"
+        fi
     fi
 done
 
